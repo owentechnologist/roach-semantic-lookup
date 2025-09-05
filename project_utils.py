ab09aac8c1380f7763aa831d8051a2abe2c04a9e
Owen Taylor <48262631+owentechnologist@users.noreply.github.com>
2025-09-05 09:54:31 -0500

    shifted location of some functions from project_utils to load_rag_data

--- project_utils.py
+++ project_utils.py
@@ -1,18 +1,23 @@
-# This is a collection of 
-# cmdline UI (and other) code
+#This is a collection of code for this project:
+''' 
+ The code defines a system for interacting with a 
+ Large Language Model (LLM), which includes functions to 
+ configure prompt templates and temperature 
+ and manage a cache of past LLM prompts and responses 
+ based on semantic similarity to improve performance and 
+ consistency.'''
 
 import sys
 
-# import to allow manipulation of json and jsonpath navigation of response from LLM:
-import json, jsonpath_ng.ext as jsonpath
 #LLM related imports:
 from sentence_transformers import SentenceTransformer
 # import to allow easy HTTP calls:
 import requests
+import json, jsonpath_ng.ext as jsonpath
 # prompt templates for LLM:
 from prompt_templates import *
 # rag text retrieval function:
-from rag_similarity_helper import *
+from rag_similarity_helper import rag_query_using_vector_similarity
 
 # import hard coded db config and localAI LLM url:
 from connection_stuff import *
@@ -33,77 +38,6 @@ def configure_temperature_and_template(template_key):
     print(f'You have set the prompt template to {template_func.__name__} the responses from the LLM will be impacted accordingly')
     
     return { "template_func": template_func, "temperature": temperature, "rag": rag}
-
-
-## this is designed to be executed before the main interactions with the LLM:
-## The data is loaded from the file: ragdata.json 
-## the sample provided could have its contents replaced with whatever makes sense for your use case
-## there is a Goldilocks effect with the length of text used in an embedding 
-# - too short and no context, too long and no focus for LLM
-# the use of the subject_matter as a filter is powerful for limiting access and setting up guard rails
-## the format used for this example is:
-'''
-{
-  "text_chunks":[
-    {"subject_matter": "public_something","content_summary_for_prompt_similarity": "This is text", "text_chunk":"the text that informs the LLM for this instance of the subject_matter"},
-    {"subject_matter": "private_something", "content_summary_for_prompt_similarity": "This is text", "text_chunk":"different text that informs the LLM for this other instance of some subject_matter"}
-  ]
-}
-'''
-def read_json_file():
-    with open('ragdata.json', 'r') as file:
-        data = json.load(file)
-        # Initialize lists to store the extracted data
-    subject_matters = []
-    similarity_texts = []
-    text_chunks = []
-    # Access the list of dictionaries using the key "text_chunks"
-    for entry in data.get("text_chunks", []):        
-        # Extract the values and append them to the respective lists
-        similarity_texts.append(entry.get('content_summary_for_prompt_similarity', ''))
-        subject_matters.append(entry.get('subject_matter', ''))
-        text_chunks.append(entry.get('text_chunk', ''))    
-        
-    return subject_matters, similarity_texts, text_chunks
-
-# here we create the embeddings and load the data from the json file into the database: 
-def insert_text_chunk(text_embedding,subject_matter,similarity_text,text_chunk):
-    if isinstance(subject_matter, list):
-        subject_matter = ' '.join(str(x) for x in subject_matter)
-    subject_matter = subject_matter.strip()
-    if isinstance(text_chunk, list):
-        text_chunk = ' '.join(str(x) for x in text_chunk)
-    if isinstance(similarity_text, list):
-        similarity_text = ' '.join(str(x) for x in similarity_text)
-    text_chunk = text_chunk.strip()
-    similarity_text = similarity_text.strip()
-    new_pk=None
-    query = f'''INSERT INTO vdb.llm_enrichment 
-        (chunk_embedding, subject_matter, similarity_text,text_chunk)
-        VALUES ('{text_embedding}', %s, %s, %s) returning pk;'''
-    args = (subject_matter, similarity_text,text_chunk)
-    try:
-        with get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(query,args)
-                new_pk = cur.fetchone()[0] 
-    except Exception as e:
-        print(f"❌ DB Error during insert_text_chunk processing: {e}")
-    return new_pk
-
-# used as initialization through the use of the LOAD instruction 
-# (commandline only) 
-#  or if additional text chunks are added to the json file:
-# to prevent duplicates this simple sql can be used:
-#  delete from vdb.llm_enrichment where 1=1
-def load_augmentation_text():
-    subject_matters, similarity_texts, text_chunks = read_json_file()
-    for i in range(len(subject_matters)):
-        similarity_text=similarity_texts[i]
-        search_embedding = create_embedding(similarity_text)
-        llm_info_text=text_chunks[i]
-        insert_text_chunk(subject_matter=subject_matters[i],similarity_text=similarity_text,text_chunk=llm_info_text,text_embedding=search_embedding)
-    return 
 
 # a helper function that takes in a string and returns a vector embedding suitable for 
 # storing in any vectorDB (CRDB or postgres etc) as a searchable vector    
